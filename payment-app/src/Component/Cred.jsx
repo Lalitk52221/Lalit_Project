d5997538cb66d0d439690834cb03a76f9fa66fcf
Lalitk52221 <lalitk52221@outlook.com>
2024-06-01 20:21:44 +0530

    .

---
+++ payment-app/src/Component/Cred.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cred = () => {
+  return (
+    <div className='text-white'>
+      WElcome to Cred 
+    </div>
+  )
+}
+
+export default Cred
---
+++ payment-app/src/Component/FormContext.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+ export const FormContext = createContext();
+
+export const FormProvider = ({children})=>{
+
+const [formData, setFormData] = useState({
+    name:'',
+    amount:'',
+    date: new Date(),
+    time:'',
+    amtinRs:'',
+    payname:'',
+    BankingName:'',
+    phone:''
+})
+
+
+    return (
+        <FormContext.Provider value={{formData,setFormData,}}>
+            {children}
+        </FormContext.Provider>
+    )
+}
